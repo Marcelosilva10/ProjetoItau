719060fdc2294996b2538367f59c0ef602ca7eef
Marcelo Henrique <marcelo-h-silva@hotmail.com>
2022-03-24 00:21:22 -0300

    update projeto gabriel

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FormularioComponent } from './component/formulario/formulario.component';
+import { ListagemComponent } from './component/listagem/listagem.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'formulario', component: FormularioComponent },
+  { path: 'listagem', component: ListagemComponent },
+  { path: '**', pathMatch: 'full', redirectTo: 'formulario' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListagemComponent } from './component/listagem/listagem.component';
+import { FormularioComponent } from './component/formulario/formulario.component';
+import { NavbarComponent } from './model/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListagemComponent,
+    FormularioComponent,
+    NavbarComponent,
   ],
   imports: [
     BrowserModule,
